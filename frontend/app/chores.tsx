9f8861bf736432a2ec23ce951751ea31ca6bd6e6
Joutee <kovandapk@gmail.com>
2026-01-24 19:55:05 +0100

    initial setup

---
+++ frontend/app/chores.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const chores = () => {
+  return (
+    <View>
+      <Text>chores</Text>
+    </View>
+  )
+}
+
+export default chores
+
+const styles = StyleSheet.create({})
---
+++ frontend/app/flat.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const flat = () => {
+  return (
+    <View>
+      <Text>flat</Text>
+    </View>
+  )
+}
+
+export default flat
+
+const styles = StyleSheet.create({})
